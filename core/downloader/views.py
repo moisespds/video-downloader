8947f466f1f87f2acceb7075ff529a0e7f810a27
moisespds <m.pradodasilva@gmail.com>
2026-02-21 18:17:18 -0300

    refactor: add test and error message

--- core/downloader/views.py
+++ core/downloader/views.py
@@ -10,14 +10,18 @@ import os
 
 # Create your views here.
 def home(request):
+    print("Método:", request.method)
     if request.method == "POST":
         video_url = request.POST.get('url')
-
-    try:
-        filepath = download_video(video_url)
-
-        return FileResponse(open(filepath, 'rb'), as_attachment=True)
-    except Exception:
-        return render(request, "downloader/home.html", { "error": "Error found while downloading the video. Please check the URL and try again."
-         })
-        
+        print("URL:", video_url)
+        # ...existing code...
+        try:
+            if not video_url:
+                return render(request, 'downloader/home.html', {'error': 'Informe uma URL'})
+            filepath = download_video(video_url)
+            return FileResponse(open(filepath, 'rb'), as_attachment=True)
+        except Exception:
+            return render(request, "downloader/home.html", { "error": "Error found while downloading the video. Please check the URL and try again." })
+    else:
+        return render(request, "downloader/home.html")
+    
